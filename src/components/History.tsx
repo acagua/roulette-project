c81a2e2c4102bb845bfd182b619f47bcd53defe1
Andres Cagua <af.cagua1011@gmail.com>
2022-07-07 18:04:30 -0500

    add send hystory numbers button

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -7,22 +7,44 @@ import { IRouletteState } from "../redux/reducers/rouletteReducer";
 import { rouletteActions } from "../redux/actions/rouletteActions";
 import { Button } from "./Button";
 import { ReactComponent as TrashIcon } from "../assets/trash.svg";
+import { ReactComponent as PaperPlaneIcon } from "../assets/paperplane.svg";
+import emailjs from "@emailjs/browser";
 import { colorMapper } from "../utils/board";
+import { useRef } from "react";
 
 export const History = () => {
   const { history }: IRouletteState = useSelector(
     (state: RootState) => state.roulette
   );
+  const historyList = useRef();
 
-
-  
+  const handleSendHistory = (e) => {
+    // e.preventDefault(); // Prevents default refresh by the browser
+    // emailjs
+    //   .sendForm(
+    //     import.meta.env.EMAILJS_SERVICE_ID,
+    //     import.meta.env.EMAILJS_TEMPLATE_ID,
+    //     historyList.current,
+    //     import.meta.env.EMAILJS_PUBLIC_KEY
+    //   )
+    //   .then(
+    //     (result) => {
+    //       alert("Message Sent, We will get back to you shortly");
+    //       console.log("YAAAAS");
+    //     },
+    //     (error) => {
+    //       alert("An error occurred, Please try again");
+    //       console.log("FAAAAAAK");
+    //     }
+    //   );
+  };
 
   return (
     <section className="history">
       <div className="black-number" key="recent">
         Hit
       </div>
-      <ul className="history-list">
+      <form ref={historyList} className="history-list">
         {history.map(({ numberInfo, id }) => (
           <li className={`${colorMapper[numberInfo.redBlack]}-number`} key={id}>
             {numberInfo.number}
@@ -31,7 +53,7 @@ export const History = () => {
         <li className="black-number" key="recent">
           End
         </li>
-      </ul>
+      </form>
 
       <Button
         Icon={TrashIcon}
@@ -39,6 +61,14 @@ export const History = () => {
         disabled={history.length === 0}
         colorType="warning"
       />
+
+      <button
+        className={`ok-button`}
+        onDoubleClick={handleSendHistory}
+        disabled={history.length < 30}
+      >
+        <PaperPlaneIcon fill="white" />
+      </button>
     </section>
   );
 };
